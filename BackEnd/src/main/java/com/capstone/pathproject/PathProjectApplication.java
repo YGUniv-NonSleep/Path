708df8c43e55a98f5e7e3f0cf38948aae941ee06
Sangyong-Jeon <tkddyd420@naver.com>
2022-02-17 15:35:21 +0900

    초기셋팅

---
+++ BackEnd/src/main/java/com/capstone/pathproject/PathProjectApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.pathproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathProjectApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/capstone/pathproject/PathProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.pathproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
