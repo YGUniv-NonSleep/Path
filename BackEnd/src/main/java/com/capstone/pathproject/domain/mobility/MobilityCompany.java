6075784d525cde3022cc734e3eff201190316da5
Sangyong-Jeon <tkddyd420@naver.com>
2022-03-14 13:36:24 +0900

    [백] 물리명 변경
    물리명 약어로 변경

--- BackEnd/src/main/java/com/capstone/pathproject/domain/mobility/MobilityCompany.java
+++ BackEnd/src/main/java/com/capstone/pathproject/domain/mobility/MobilityCompany.java
@@ -18,30 +18,30 @@ public class MobilityCompany {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MOBILITY_COMPANY_SEQ_GENERATOR")
-    @Column(name = "MOBILITY_CO_ID")
+    @Column(name = "MOBIL_CO_ID")
     private Long id;
 
-    @Column(name = "MOBILITY_CO_NAME")
+    @Column(name = "MOBIL_CO_NAME")
     private String name;
 
-    @Column(name = "MOBILITY_CO_CRN")
+    @Column(name = "MOBIL_CO_CRN")
     private String companyNumber;
 
-    @Column(name = "MOBILITY_CO_OPEN_DATE")
+    @Column(name = "MOBIL_CO_OPEN_DATE")
     private LocalDate openDate;
 
-    @Column(name = "MOBILITY_CO_MAIL")
+    @Column(name = "MOBIL_CO__MAIL")
     private String mail;
 
-    @Column(name = "MOBILITY_CO_CEO_NAME")
+    @Column(name = "MOBIL_CO_CEO_NAME")
     private String ceoName;
 
-    @Column(name = "MOBILITY_CO_CEO_PHONE")
+    @Column(name = "MOBIL_CO__CEO_PHONE")
     private String ceoPhone;
 
-    @Column(name = "MOBILITY_CO_UNLOCK_FEE")
+    @Column(name = "MOBIL_CO_UNLOCK_FEE")
     private String unlockFee;
 
-    @Column(name = "MOBILITY_CO_MINUTE_FEE")
+    @Column(name = "MOBIL_CO_MINUTE_FEE")
     private String minuteFee;
 }
