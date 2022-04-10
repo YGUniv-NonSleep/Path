eaacd88ad75e37a477644b0ceb4a45bbdcd4c520
Sangyong-Jeon <tkddyd420@naver.com>
2022-04-10 13:15:25 +0900

    [프론트] 로그인 페이지 수정
    - css 기능 수정
    - js 기능 수정

--- front-end/src/routes/Member/Login/LoginPresenter.js
+++ front-end/src/routes/Member/Login/LoginPresenter.js
@@ -10,9 +10,12 @@ import {
   Box,
   Typography,
   Container,
+  FormControl,
+  FormHelperText,
 } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import styled from 'styled-components';
 
 function Copyright(props) {
   return (
@@ -30,9 +33,18 @@ function Copyright(props) {
   );
 }
 
+const FormHelperTexts = styled(FormHelperText)`
+  width: 100%;
+  padding-left: 16px;
+  font-weight: 700;
+  color: #d32f2f;
+`;
+
 const theme = createTheme();
 
 function LoginPresenter(props) {
+  const { loginIdError, passwordError } = props.errorList;
+
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -51,40 +63,55 @@ function LoginPresenter(props) {
           <Typography component="h1" variant="h4">
             회원가입
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              onChange={props.handleInput}
-              id="loginId"
-              label="아이디"
-              name="loginId"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              onChange={props.handleInput}
-              name="password"
-              label="비밀번호"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              onClick={props.handleSubmit}
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={props.handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <FormControl component="fieldset" variant="standard">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    autoFocus
+                    fullWidth
+                    id="loginId"
+                    name="loginId"
+                    label="아이디"
+                    onChange={props.handleInput}
+                    error={loginIdError !== '' || false}
+                  />
+                </Grid>
+                <FormHelperTexts>{loginIdError}</FormHelperTexts>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    type="password"
+                    id="password"
+                    name="password"
+                    label="비밀번호"
+                    onChange={props.handleInput}
+                    error={passwordError !== '' || false}
+                  />
+                </Grid>
+                <FormHelperTexts>{passwordError}</FormHelperTexts>
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                size="large"
+              >
+                Sign In
+              </Button>
+            </FormControl>
             <Grid container spacing={3}>
               <Grid item xs={4}>
                 <Link to="/searchId">아이디 찾기</Link>
