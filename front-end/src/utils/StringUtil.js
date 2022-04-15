e339c2dd8f00637d666bc37280713402dd48eb73
Sangyong-Jeon <tkddyd420@naver.com>
2022-04-15 11:22:46 +0900

    [프론트] StringUtil 설명 추가

--- front-end/src/utils/StringUtil.js
+++ front-end/src/utils/StringUtil.js
@@ -1,5 +1,7 @@
 /*
-    str이 null , '', undefined 인지 확인하는 함수
+    설명 : null , '', undefined 인지 확인하는 함수
+    param : 전체 타입
+    return : boolean
 */
 export const isEmpty = (str) => {
   if (typeof str == 'undefined' || str == null || str == '') return true;
