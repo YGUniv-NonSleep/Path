2748007aedc2e330a5905d21c0a4e511e83a4358
sieun <tldms521@naver.com>
2022-04-20 19:08:17 +0900

    commit

--- front-end/src/components/Icon.js
+++ front-end/src/components/Icon.js
@@ -11,17 +11,21 @@ import {
   DirectionsCarFilled,
 } from "@mui/icons-material";
 import IconButton from "@mui/material/IconButton";
+
 // https://blog.logrocket.com/react-router-v6-future-reach-router/
 // https://velog.io/@wiostz98kr/TIL50-l-React-Router-match.params-location.search
 // http://www.tcpschool.com/css/css3_transform_2Dtransform
+
 const Ul = styled.ul`
   position: relative;
   display: grid;
   grid-template-columns: 50px 50px 50px 50px 50px 50px 50px;
 `;
+
 const Li = styled.li`
   text-align: center;
 `;
+
 const Icon = () => {
   return (
     <Ul>
@@ -31,36 +35,42 @@ const Icon = () => {
         </IconButton>
         <Typography variant="subtitle2">통합</Typography>
       </Li>
+
       <Li>
         <IconButton color="primary">
           <DirectionsBus />
         </IconButton>
         <Typography variant="subtitle2">버스</Typography>
       </Li>
+
       <Li>
         <IconButton color="primary">
           <DirectionsSubway />
         </IconButton>
         <Typography variant="subtitle2">지하철</Typography>
       </Li>
+
       <Li>
         <IconButton color="primary">
           <ElectricScooter />
         </IconButton>
         <Typography variant="subtitle2">킥보드</Typography>
       </Li>
+
       <Li>
         <IconButton color="primary">
           <PedalBike />
         </IconButton>
         <Typography variant="subtitle2">자전거</Typography>
       </Li>
+
       <Li>
         <IconButton color="primary">
           <DirectionsWalk />
         </IconButton>
         <Typography variant="subtitle2">도보</Typography>
       </Li>
+
       <Li>
         <IconButton color="primary">
           <DirectionsCarFilled />
@@ -70,4 +80,5 @@ const Icon = () => {
     </Ul>
   );
 };
+
 export default Icon;
