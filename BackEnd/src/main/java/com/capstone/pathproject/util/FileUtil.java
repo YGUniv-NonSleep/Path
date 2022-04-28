04ce2d50d9d54d548798dde0583870e382cdc1a4
Sangyong-Jeon <tkddyd420@naver.com>
2022-04-28 18:52:57 +0900

    [백] File 테스트 삭제 및 수정
    - File 관련한 함수는 Util 클래스 만들어서 공통적으로 사용하기로 결정

---
+++ BackEnd/src/main/java/com/capstone/pathproject/util/FileUtil.java
@@ -0,0 +1,52 @@
+package com.capstone.pathproject.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class FileUtil {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<String> storeFiles(List<MultipartFile> multipartFiles) {
+        List<String> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    private String storeFile(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) return null;
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        try {
+            multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        } catch (IOException e) {
+            log.error("파일 저장 실패 : IOException 발생", e);
+        }
+        return storeFileName;
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        return LocalDateTime.now() + originalFilename;
+    }
+}
