48a6ebe73da6cc14ef857f79575a574ec86f0724
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-12 16:18:58 +0900

    [백] 퍼스널모빌리티 조회 성능 개선
    - 기존 연관관계인 제휴업체를 계속 조회날리는 N+1 문제가 생겨서 처음 조회할 때 left join을 시켜 쿼리 한방에 해결함.

--- BackEnd/src/main/java/com/capstone/pathproject/repository/mobility/MobilityRepository.java
+++ BackEnd/src/main/java/com/capstone/pathproject/repository/mobility/MobilityRepository.java
@@ -1,6 +1,7 @@
 package com.capstone.pathproject.repository.mobility;
 
 import com.capstone.pathproject.domain.mobility.Mobility;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,7 @@ import java.util.List;
 public interface MobilityRepository extends JpaRepository<Mobility, Long> {
 
     // 500m 범위 x경도(Long) 0.0055, y위도(Lat) 0.00475,
+    @EntityGraph(attributePaths = {"mobilityCompany"})
     @Query("select m from Mobility m " +
             "where (m.longitude between :x - 0.0055 and :x + 0.0055) " +
             "and (m.latitude between :y - 0.00475 and :y + 0.00475) " +
