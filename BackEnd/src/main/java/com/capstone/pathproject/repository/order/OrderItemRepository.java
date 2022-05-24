268bf755a328740217ba1c0934982d2357508033
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-24 11:49:12 +0900

    [백] 테이블 변경에 따른 repository 이름 변경

--- BackEnd/src/main/java/com/capstone/pathproject/repository/order/OrderItemRepository.java
+++ BackEnd/src/main/java/com/capstone/pathproject/repository/order/OrderItemRepository.java
@@ -3,6 +3,6 @@ package com.capstone.pathproject.repository.order;
 import com.capstone.pathproject.domain.order.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompositionRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     
 }
