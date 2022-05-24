252ed231da0629b04bed725b302f8d5fdc2f8038
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-24 09:58:35 +0900

    [프론트] Redux 회원 정보 추가
    회원 id 값 추가

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -4,12 +4,14 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 let user = createSlice({
   name: 'user',
   initialState: {
+    id: 0,
     name: 'anonymous',
     loginId: 'anonymous',
     role: 'ROLE_ANONYMOUS',
   },
   reducers: {
     changeUser(state, action) {
+      state.id = action.payload.id;
       state.name = action.payload.name;
       state.loginId = action.payload.loginId;
       state.role = action.payload.role;
