46cec91b4a8ccd968205d136902f9a42b4eb9e4d
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-24 11:46:21 +0900

    [백] ORDER_ITEM, ORDER_ITEM_OPTION 테이블 이름 변경
    composition -> OrderItem
    OptionComposition -> OrderItemOption

--- BackEnd/src/main/java/com/capstone/pathproject/repository/order/CompositionRepository.java
+++ BackEnd/src/main/java/com/capstone/pathproject/repository/order/CompositionRepository.java
@@ -1,8 +1,8 @@
 package com.capstone.pathproject.repository.order;
 
-import com.capstone.pathproject.domain.order.Composition;
+import com.capstone.pathproject.domain.order.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompositionRepository extends JpaRepository<Composition, Long> {
+public interface CompositionRepository extends JpaRepository<OrderItem, Long> {
     
 }
