851d2e367e80022c675350cec8a1ea626644994e
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-25 13:29:22 +0900

    [프론트] 차량 관리 페이지 및 등록 모달창 추가
    - 차량 관리 페이지 추가
    - 차량 등록 모달창 추가

--- front-end/src/pages/Member/index.js
+++ front-end/src/pages/Member/index.js
@@ -1,21 +1,10 @@
 import Member from './MemberMain';
 import Card from './Card/Card';
 import Cars from './Cars/Cars';
-import AddCars from './Cars/addCars';
 import Login from './Login/LoginMain';
 import SignUp from './SignUp/SignUpMain';
 import UpdateMem from './Update/UpdateMemMain';
 import SearchId from './SearchId/SearchIdMain';
 import SearchPw from './SearchPw/SearchPwMain';
 
-export {
-  Member,
-  Card,
-  Cars,
-  AddCars,
-  Login,
-  SignUp,
-  UpdateMem,
-  SearchId,
-  SearchPw,
-};
+export { Member, Card, Cars, Login, SignUp, UpdateMem, SearchId, SearchPw };
