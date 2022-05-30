0da3a0bec43b6cf14d7e11d37045569ad7e84012
ovadozz <endgus112@g.yju.ac.kr>
2022-05-30 11:12:33 +0900

    [백,프론트] 카풀 등록시 차량추가
    [백,프론트] 카풀 등록시 차량추가

--- front-end/src/pages/CarPool/CarPoolAdd.js
+++ front-end/src/pages/CarPool/CarPoolAdd.js
@@ -6,6 +6,8 @@ import {
 } from "@mui/icons-material";
 import useCarPoolAdd from "./hooks/useCarPoolAdd";
 
+
+
 const CommuCon = styled.div`
   width: 390px;
   height: 100%;
@@ -15,10 +17,43 @@ const CommuSubCon = styled.div`
   margin-left: 130px;
 `;
 
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 0;
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  max-height: 80%;
+  width: 20rem;
+  height: 80%;
+  padding: 16px;
+  background: rgb(25, 31, 44);
+  border-radius: 10px;
+  text-align: center;
+`;
+
+const MousePointer = styled.div`
+  cursor: pointer;
+  :hover {
+    color: white;
+  }
+`;
+
+
+
 function CarPoolAdd() {
   const { 
-    isStartOpen, isArrivedOpen, startAddr, arriveAddr, 
-    createCarPost, openStartCode, openArrivedCode, handleComplete, handleComplete2
+    isStartOpen, isArrivedOpen, startAddr, arriveAddr, memberCars,caropenModal,choiceCarKind,choiceCarNum,
+    createCarPost, openStartCode, openArrivedCode, handleComplete, handleComplete2,findCarsModal,closeCarsModal,choiceMyCar
   } = useCarPoolAdd();
 
   return (
@@ -31,6 +66,33 @@ function CarPoolAdd() {
             시작날짜 : <input type="date" name="sdate" /><br></br>
             종료날짜 : <input type="date" name="edate" /><br></br>
             출발시간 : <input type="time" name="stime" /><br></br>
+            <button onClick={findCarsModal}>내 차 찾기</button><br></br>
+            {caropenModal ? (
+              <div>
+               <Background>
+                 <ModalContainer>
+                  <MousePointer
+                   style={{ color: "white", float: "right" }}
+                    onClick={closeCarsModal}
+                   >
+                   X
+                  </MousePointer>
+                  <div style={{ color: "white", float: "center" }}>내 차 찾기</div>
+                  <div style={{ color : "white"}}>
+                  {memberCars.map((cars,index)=>(
+                    <span key={index} onClick={(e)=>{choiceMyCar(cars,e)}}>
+                      <img src={`${process.env.REACT_APP_SPRING_API}/api/image/${cars.photoName}`} style={{width : "250px",height : "150px"}}></img><br></br>
+                      <p name="carKind">{cars.carKind}</p><p>{cars.carNum}</p><br></br>
+                    </span>
+                  ))}
+                  </div>
+                 </ModalContainer>
+               </Background>
+              </div>
+            ) : ("") 
+            }
+            <p>{choiceCarKind}</p>
+            <p>{choiceCarNum}</p> 
             <button type="button" onClick={openStartCode}>출발지 검색</button>
             <p>{startAddr}</p>
             <button type="button" onClick={openArrivedCode}>도착지 검색</button>
--- front-end/src/pages/CarPool/hooks/useCarPoolAdd.js
+++ front-end/src/pages/CarPool/hooks/useCarPoolAdd.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 function useCarPoolAdd() {
   
@@ -21,10 +22,31 @@ function useCarPoolAdd() {
   const [startLocal2, setStartLocal2] = useState(null);
   const [arriveLocal1, setArriveLocal1] = useState(null);
   const [arriveLocal2, setArriveLocal2] = useState(null);
-
-
+  const [memberCars, setMemberCars] = useState(null);
+  const [caropenModal, setCarOpenModal] = useState(false);
+  const [carChoose, setCarChoose] = useState(null);
+  
+  const [carsId, setCarsId] = useState(null);
+  const [choiceCarKind, setChoiceCarKind] = useState(null);
+  const [choiceCarNum, setChoiceCarNum] = useState(null);
+  
   let navigate = useNavigate();
   let geocoder = new kakao.maps.services.Geocoder();
+  let state = useSelector((state) => state);
+
+ 
+  useEffect(()=>{
+    var memberId = state.user.id;
+    console.log(typeof memberId);
+    axios.get(process.env.REACT_APP_SPRING_API + `/api/cars/view?memberId=${memberId}`)
+    .then((res)=>{
+      console.log(res.data.body)
+      setMemberCars(res.data.body);
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  },[state])
 
   function getCoords(data) {
     geocoder.addressSearch(data, function (result, status) {
@@ -72,7 +94,7 @@ function useCarPoolAdd() {
     }
     console.log(data)
     setStartLocal1(data.sigungu);
-    setStartLocal2(data.banme);
+    setStartLocal2(data.bname);
     setStartAddr(fullAddress);
     getCoords(fullAddress);
   };
@@ -103,8 +125,30 @@ function useCarPoolAdd() {
     getArrivedCoords(fullAddress);
   };
 
+  const findCarsModal = (e) => {
+    e.preventDefault();
+    setCarOpenModal(true);
+  }
+
+  const closeCarsModal = (e) => {
+    e.preventDefault();
+    setCarOpenModal(false)
+  }
+  
+  const choiceMyCar = (cars,e) => {
+    e.preventDefault();
+    setCarsId(cars.id);
+    setChoiceCarKind(cars.carKind);
+    setChoiceCarNum(cars.carNum);
+    alert("선택완료");
+    closeCarsModal(e);
+    //console.log(e.target.carKind.value)
+  }
+
+
   const createCarPost = (e) => {
     e.preventDefault();
+  
     var data = {
       title: e.target.title.value,
       content: e.target.content.value,
@@ -120,9 +164,9 @@ function useCarPoolAdd() {
       startLocal2 : startLocal2,
       arriveLocal1 : arriveLocal1,
       arriveLocal2 : arriveLocal2,
-      cars: {
-        id: 1,
-      },
+      cars : {
+        id : carsId
+      }
     };
     console.log(data);
 
@@ -154,9 +198,9 @@ function useCarPoolAdd() {
   };
 
   return { 
-    isStartOpen, isArrivedOpen, dataset, startAddr, arriveAddr, 
+    isStartOpen, isArrivedOpen, dataset, startAddr, arriveAddr, memberCars,caropenModal,choiceCarKind,choiceCarNum,
     navigate, getCoords, getArrivedCoords, openStartCode, openArrivedCode, 
-    handleComplete, handleComplete2, createCarPost 
+    handleComplete, handleComplete2, createCarPost,findCarsModal,closeCarsModal,choiceMyCar
   }
 }
 
