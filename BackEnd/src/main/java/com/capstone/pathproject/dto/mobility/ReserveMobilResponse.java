1dff120dea94c7c3dbf2facc061000b21fbf51dd
Sangyong-Jeon <tkddyd420@naver.com>
2022-06-14 11:17:14 +0900

    [백] 퍼스널 모빌리티 응답값 수정
    - 응답할 때 변수명 수정

--- BackEnd/src/main/java/com/capstone/pathproject/dto/mobility/ReserveMobilResponse.java
+++ BackEnd/src/main/java/com/capstone/pathproject/dto/mobility/ReserveMobilResponse.java
@@ -2,7 +2,6 @@ package com.capstone.pathproject.dto.mobility;
 
 import com.capstone.pathproject.domain.mobility.Mobility;
 import com.capstone.pathproject.domain.mobility.MobilityReserveResult;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,13 +15,13 @@ import java.time.format.DateTimeFormatter;
 public class ReserveMobilResponse {
 
     private Long reserveId;
-    private LocationMobilityDto mobilityDto;
     private MobilityReserveResult result;
     private String reserveDateTime;
+    private LocationMobilityDto mobility;
 
     public ReserveMobilResponse(Long reserveId, Mobility mobility, MobilityReserveResult result, LocalDateTime reserveLocalDateTime) {
         this.reserveId = reserveId;
-        this.mobilityDto = new LocationMobilityDto(mobility);
+        this.mobility = new LocationMobilityDto(mobility);
         this.result = result;
         this.reserveDateTime = reserveLocalDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
