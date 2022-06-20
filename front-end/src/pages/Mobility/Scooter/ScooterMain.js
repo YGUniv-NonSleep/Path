9b75245e4ad612a12d28eea47346c39d437368bb
sieun <tldms521@naver.com>
2022-06-20 18:41:11 +0900

    모빌리티 모달창 구현

--- front-end/src/pages/Mobility/Scooter/ScooterMain.js
+++ front-end/src/pages/Mobility/Scooter/ScooterMain.js
@@ -4,14 +4,16 @@ import MIcon from "../MIcon";
 import Map from "../../../components/Map";
 import useLoading from '../../../hooks/useLoading';
 import useScooterIcon from '../hooks/useScooterIcon';
-import Modal from "../../../components/Modal";
+import BatteryIcon from '@mui/icons-material/Battery60';
+import { DonutLarge } from "@mui/icons-material";
 
 const SideNav = styled.nav`
   position: fixed;
   left: 95px;
   z-index: 5;
   background-color: white;
-  box-shadow: 3px 3px 3px gray;
+  border-radius : 2px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   width: 380px;
   height: 90px;
 `;
@@ -23,18 +25,116 @@ const BarContainer = styled.div`
   margin-top: 90px;
 `;
 
+const Modal = styled.div`
+position: absolute;
+left: 600px;
+top: 720px;
+width: 460px;
+height: 180px;
+background-color: white;
+border-radius : 10px;
+box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+const ScootNum = styled.div`
+position: relative;
+top: 10px;
+left: 10px;
+font-size: 15px;
+`;
+
+const RunTime = styled.div`
+position: relative;
+top: 20px;
+left: 10px;
+font-size: 15px;
+`;
+
+const UnlockCost = styled.div`
+position: relative;
+bottom: 45px;
+left: 345px;
+font-size: 14px;
+`;
+
+const MinuteCost = styled.div`
+position: relative;
+left: 378px;
+bottom: 35px;
+font-size: 14px;
+`;
+
+const UseStart = styled.button`
+position: relative;
+top: 40px;
+left: 75px;
+width: 150px;
+height: 40px;
+border-radius: 15px;
+border: none;
+outline:none;
+background-color: #1ABC9C;
+  &:hover {
+    background-color: #00A98F;
+  };
+`;
+
+const Reserve = styled.button`
+position: relative;
+top: 40px;
+left: 90px;
+width: 150px;
+height: 40px;
+border-radius: 15px;
+border: none;
+outline:none;
+background-color: #1ABC9C;
+&:hover {
+  background-color: #00A98F;
+}
+`;
+
+const Battery = styled.div`
+position: relative;
+top: 15px;
+left: 5px;
+width: 100px;
+height: 30px;
+font-size: 11px;
+`;
+
+const Num = styled.div`
+position: relative;
+bottom: 20px;
+left: 30px;
+font-size: 11px;
+color: gray;
+`;
+
 function ScooterMain() {
   const { loading } = useLoading();
-  const { open, handleClose } = useScooterIcon();
+  const { open, handleClose, modal} = useScooterIcon();
 
   return (
     <div className="Mobility">
       <SideNav>
         <BarContainer>
           <MIcon />
-          
         </BarContainer>
-
+        {modal == "open" ? 
+        
+        <Modal>
+          <ScootNum>#19995</ScootNum>
+          <Battery><BatteryIcon sx={{fontSize: 30, color: "#00A98F"}} /><Num>60%</Num></Battery>
+          <RunTime>17km 주행 가능</RunTime>
+          <UnlockCost>잠금 해제 300</UnlockCost>
+          <MinuteCost>분당 150</MinuteCost>
+          <UseStart>이용 시작</UseStart>
+          <Reserve>예약 하기</Reserve>
+        </Modal> 
+        
+        : <></>}
+        
         
       </SideNav>
      
