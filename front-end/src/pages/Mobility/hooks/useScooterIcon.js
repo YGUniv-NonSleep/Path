a19bfa07c14900db1664f297a3df47d14b1088b3
sieun <tldms521@naver.com>
2022-07-06 10:54:51 +0900

    퍼스널모빌리티

--- front-end/src/pages/Mobility/hooks/useScooterIcon.js
+++ front-end/src/pages/Mobility/hooks/useScooterIcon.js
@@ -2,13 +2,22 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import MapApi from "../../../api/MapApi";
 import scooter from "../../../assets/images/electric-scooter.png";
+import parking from "../../../assets/images/parking.png";
+import gps from "../../../assets/images/placeholder.png";
 
 function useScooterIcon(){
   const [map, settingMap] = useState(null);
   const [modal, setModal] = useState(null);
-  const [saveData, setSaveData] = useState('');
+  const [modalRes, setModalRes] = useState(null);
+  const [startMod, setStartMod] = useState('');
   const [mobilData, setMobilData] = useState([]);
   const [mobilMarker, setMobilMarker] = useState([]);
+  const [minutes, setMinutes] = useState(15);
+  const [seconds, setSeconds] = useState(0);
+  const [parkData, setParkData] = useState([]);
+  const [markers, setMarkers] = useState([]);
+  const [send, setSend] = useState([]);  
+ 
 
   async function mapLoad() {
     try {
@@ -30,6 +39,13 @@ function useScooterIcon(){
     if (open === true) return setOpen(false);
   };
 
+  function reserve(e) {
+    console.log(e.target.value);
+    setModal(null);
+    setStartMod('');
+    setModalRes("openRes")
+  }
+
   const getMobilities = async (type, x, y) => {
     const data = {
       x: x,
@@ -43,12 +59,73 @@ function useScooterIcon(){
       })
       .catch((err) => console.log(err));
     return response;
+  };  
+  
+  function useStart(param, e) {
+    console.log(e.target.value);
+    let imageSrc = parking;
+    let imageGps = gps;
+    console.log(param);
+    setSend(param);
+    removeMarkers();
+    setStartMod("open");
+    setModalRes(null);
+    setModal(null);
+
+    const normalImage = new kakao.maps.MarkerImage(
+      imageSrc,
+      new kakao.maps.Size(45,45)
+    );
+
+    const useStayImg = new kakao.maps.MarkerImage(
+      imageGps,
+      new kakao.maps.Size(40,40)
+    )
+
+    var marks = new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(
+        35.8956224,
+        128.6224266
+      ),
+      image: useStayImg
+    });
+
+    for (var i = 0; i < 5; i++) {
+      var mk = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(
+          parkData.data.body[i].latitude,
+          parkData.data.body[i].longitude
+        ),
+        image: normalImage,
+        clickable: true,
+      });
+    }
+    mk.setMap(map);
+    marks.setMap(map);
+  };
+
+  function useStop(e) {
+    console.log(e.target.value); 
+    setStartMod('');
+    setModalRes(null);
+    setModal(null);
+    location.reload();
+  }
+
+  function removeMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    setMarkers([]);
   };
 
   async function ScooterIcon() {
     const responseMobil = await getMobilities('KICKBOARD', 128.621635, 35.89581752);
     console.log(responseMobil);
-    
+    setParkData(responseMobil);
+
     let imageSrc = scooter;
 
     const normalImage = new kakao.maps.MarkerImage(
@@ -66,6 +143,8 @@ function useScooterIcon(){
         image: normalImage,
         clickable: true,
       });
+
+      setMarkers((current) => [...current, marker]);
 
       var mobilData = responseMobil.data.body[i];
 
@@ -90,7 +169,6 @@ function useScooterIcon(){
     kakao.maps.event.addListener(marker, "click", makeClickListener(map, marker, mobilData));
     setMobilMarker((prev) => [...prev, marker]);
   }
-
     marker.setMap(map);
   }
 
@@ -112,6 +190,8 @@ function useScooterIcon(){
     return () => {
       setMobilData(mobilData)
       setModal("open");
+      setMinutes(15);
+      setSeconds(0);
     };
   };
 
@@ -123,8 +203,27 @@ function useScooterIcon(){
     ScooterIcon();
   }, [map]);
 
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (parseInt(seconds) > 0) {
+        setSeconds(parseInt(seconds) - 1);
+      }
+      if (parseInt(seconds) === 0) {
+        if (parseInt(minutes) === 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(parseInt(minutes) - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
+
+
   return {
-    mapLoad, ScooterIcon, handleOpen, handleClose, open, modal, mobilData
+    mapLoad, ScooterIcon, handleOpen, handleClose, open, modal, mobilData, reserve, modalRes, useStart,
+    minutes, seconds, startMod, send, useStop
   }
 }
 
